a7e53957500899d799e5559e32dbf01ae14f4f3a
Dongkeun Lee <ldkz2524@ucla.edu>
2017-01-06 16:13:28 +0000

    crawl client ct being implemented

--- KDHC/spiders/news_spider.py
+++ KDHC/spiders/news_spider.py
@@ -24,7 +24,6 @@ class NewsSpider(scrapy.Spider):
             self.api_key = settings['fcm.api_key']
 
     def get_crawl_url(self):
-        self.log(self.password)
         conn = pymysql.connect( host=self.host,
                                 user=self.user,
                                 password=self.password,
@@ -35,7 +34,27 @@ class NewsSpider(scrapy.Spider):
         try:
             with conn.cursor() as cursor:
                 # Read a single record
-                sql = "SELECT `url_id`, `url`, `keyword`, `mod_dtime` FROM `crawl_url`"
+                sql = "SELECT `url_id`, `url`, `keyword`, `mod_dtime` FROM `crawl_url` where `url_id` in (select `url_id` from `client_crawl_ct`)"
+                cursor.execute(sql)
+                result = cursor.fetchall()
+        except Exception as e:
+            self.logger.error(str(e))
+        finally:
+            conn.close()
+        return result
+
+    def get_client_crawl_ct(self):
+        conn = pymysql.connect( host=self.host,
+                                user=self.user,
+                                password=self.password,
+                                db=self.db,
+                                charset='utf8',
+                                cursorclass=pymysql.cursors.DictCursor)
+    
+        try:
+            with conn.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT `url_id`, `registration_id` FROM `client_crawl_ct`, `client` WHERE `client_crawl_ct`.`client_id` = `client`.`client_id` and `url_id` in (select `url_id` from `crawl_url`) order by `url_id` asc"
                 cursor.execute(sql)
                 result = cursor.fetchall()
         except Exception as e:
@@ -56,16 +75,12 @@ class NewsSpider(scrapy.Spider):
                                 charset='utf8mb4',
                                 cursorclass=pymysql.cursors.DictCursor)
         
-        push_service = FCMNotification(api_key=self.api_key)
-        registration_id = 'e-h1Nmafro0:APA91bHN2IZ6bd98dpgLvsNTdcTuq5UWOM3vMqaBn1y_Re0taQ4_TUhDw9gOKF0ny4KYsxM3BLOjtTureQ6Ns1Kc-e6ofV2sYkTuvDPyObw0ukWnQJgDosG1Ots2wo2S6SWZrIHeqT13'
-        
         #under the assumption that no new article (that hasn't been saved to db) will be in between old articles
         #so it commits new articles until duplicate article is found. When duplicate is found, raise integrity error
+        messages_to_be_sent = []
         try:
-            messages_to_be_sent = []
             for p in response.xpath('//item'):
                 with conn.cursor() as cursor:
-                    self.log(p.xpath('./pubDate/text()').extract_first())
                     sql = "INSERT INTO `news` (`news_url`, `crawled_url_id`, `title`, `description`, `pub_date`, `author`, `category`) " 
                     sql += "VALUES (%s, %s, %s, %s, %s, %s, %s)"
                     cursor.execute(sql, (p.xpath('./link/text()').extract_first(), 
@@ -77,12 +92,7 @@ class NewsSpider(scrapy.Spider):
                                         p.xpath('./category/text()').extract_first(),))
                     conn.commit()
                     #save messages to be sent
-                    messages_to_be_sent.append({'title' : p.xpath('./title/text()').extract_first(), 'body' : p.xpath('./description/text()').extract_first()})
-            while(len(messages_to_be_sent) != 0):
-                message = messages_to_be_sent.pop()
-                result = push_service.notify_single_device(registration_id=registration_id, message_title=message['title'], message_body=message['body'])
-                if (result['failure'] != 0):
-                    self.logger.error(result)
+                    messages_to_be_sent.append({'title' : p.xpath('./title/text()').extract_first(), 'body' : p.xpath('./description/text()').extract_first(), 'url_id' : response.meta['url_id']})
         except pymysql.IntegrityError as e:
             self.logger.info("DUPLICATE ENTRY");
             self.logger.info(str(e))
@@ -90,4 +100,14 @@ class NewsSpider(scrapy.Spider):
             self.logger.error(str(e))
         finally:
             conn.close()
+        
+        push_service = FCMNotification(api_key=self.api_key)
+        registration_ids = ['cT6TWP-QlzI:APA91bHJlL2MKmVOpoT14egNVh-M2zLLXRNC-lVLmD6EL1p6B8bGMNvwdP7GUDKEhyNI7b3JFAl4GpxhUQnnDuTRsOVjQ2ca3bYrzD6WWlpVo_3ZPdckQTsuRg6GIqqgBY4rLREF31Z5']
+        self.log(self.get_client_crawl_ct())
+        while messages_to_be_sent:
+            message = messages_to_be_sent.pop()
+            results = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message['title'], message_body=message['body'])
+            for result in results:
+                if (result['failure'] != 0):
+                    self.logger.error(result)
 
