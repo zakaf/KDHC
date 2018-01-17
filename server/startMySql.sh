a5c353cc590bf932ae76fee1931d0541cd0a6ef0
Dongkeun Lee <ldkz2524@ucla.edu>
2018-01-17 12:43:03 +0000

    migrated to new AWS account

--- server/startMySql.sh
+++ server/startMySql.sh
@@ -2,4 +2,6 @@
 
 #set password in ~/.my.cnf file
 
-mysql -h kdhc.civdal6dzkrp.ap-northeast-2.rds.amazonaws.com -u kdhcadmin kdhc
+#mysql -h kdhc.civdal6dzkrp.ap-northeast-2.rds.amazonaws.com -u kdhcadmin kdhc
+
+mysql -h kdhc.cen4qosj2act.ap-northeast-2.rds.amazonaws.com -u kdhcadmin kdhc
