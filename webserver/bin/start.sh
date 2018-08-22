161baa00ac573afa31d33efe0858782f29d4b493
Dongkeun Lee <ldkz2524@ucla.edu>
2018-08-22 16:02:08 +0000

    added systemd support for kdhc-webserver

---
+++ webserver/bin/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+today=`date '+%Y%m%d'`;
+
+. /home/ubuntu/.nvm/nvm.sh
+
+npm install --production
+
+npm start > ../logs/$today.log 2>&1
--- webserver/startWebServer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-today=`date '+%Y%m%d'`;
-
-npm install --production
-
-npm start > logs/$today.log 2>&1 &
-
